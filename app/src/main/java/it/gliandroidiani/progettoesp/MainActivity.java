351f375147de75951b5a83541c9504418370c560
nico9779 <nicola.levorato.2@studenti.unipd.it>
2019-05-19 21:14:37 +0200

    Aggiunto Bottom Navigation Bar

---
+++ app/src/main/java/it/gliandroidiani/progettoesp/MainActivity.java
@@ -0,0 +1,46 @@
+package it.gliandroidiani.progettoesp;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation_bar);
+        final AlarmFragment alarmFragment = new AlarmFragment();
+        final NoteFragment noteFragment = new NoteFragment();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id){
+                    case R.id.alarm:
+                        setFragment(alarmFragment);
+                        return true;
+                    case R.id.note:
+                        setFragment(noteFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        navigationView.setSelectedItemId(R.id.alarm);
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
