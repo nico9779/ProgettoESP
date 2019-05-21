1942dfe88bf9dc86f6ca17991a76a836507c114a
ElDy96 <45422271+ElDy96@users.noreply.github.com>
2019-05-21 15:10:49 +0200

    Aggiungo l'activity addAllarm

---
+++ app/src/main/java/it/gliandroidiani/progettoesp/ActivityAddAlarm.java
@@ -0,0 +1,13 @@
+package it.gliandroidiani.progettoesp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAddAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_alarm);
+    }
+}
