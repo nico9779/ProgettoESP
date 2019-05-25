f523e006ed5de437bf01751bf7e4d6733afab818
nico9779 <nicola.levorato.2@studenti.unipd.it>
2019-05-25 21:54:59 +0200

    Aggiunto TimePicker all'activity AddAlarm

--- app/src/main/java/it/gliandroidiani/progettoesp/ActivityAddAlarm.java
+++ app/src/main/java/it/gliandroidiani/progettoesp/ActivityAddAlarm.java
@@ -1,15 +1,23 @@
 package it.gliandroidiani.progettoesp;
 
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class ActivityAddAlarm extends AppCompatActivity {
+import java.util.Calendar;
+
+public class ActivityAddAlarm extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     Toolbar alarmToolbar;
+    TextView time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +27,14 @@ public class ActivityAddAlarm extends AppCompatActivity {
 
         alarmToolbar = findViewById(R.id.alarm_toolbar);
         setSupportActionBar(alarmToolbar);
+
+        time = findViewById(R.id.time);
+
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        time.setText(hour+":"+minute);
     }
 
     //Metodo che aggiunge il menu alla toolbar
@@ -34,13 +50,25 @@ public class ActivityAddAlarm extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         int selected = item.getItemId();
         if(selected == R.id.save_alarm) {
-            Toast.makeText(this, R.string.event_save_alarm, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.event_save_alarm, Toast.LENGTH_SHORT).show();
             return true;
         }
         else if(selected == R.id.cancel_alarm) {
-            Toast.makeText(this, R.string.event_cancel_alarm, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.event_cancel_alarm, Toast.LENGTH_SHORT).show();
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView textView = findViewById(R.id.time);
+        textView.setText(hourOfDay+":"+minute);
+    }
+
+    public void setTime(View view) {
+        DialogFragment timePicker = new TimePickerFragment();
+        timePicker.show(getSupportFragmentManager(), "Time Picker");
     }
 }
